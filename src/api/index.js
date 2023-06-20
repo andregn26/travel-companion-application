bf15f81702e4bea1dc20acdefe1c5d54c56c6ce3
André Gregório <andregn26@gmail.com>
2023-06-20 16:46:06 +0100

    alterar

--- src/api/index.js
+++ src/api/index.js
@@ -36,32 +36,6 @@ export const getWeatherData = async (lat, lng) => {
 					units: "metric",
 				},
 				headers: {
-					"X-RapidAPI-Key":
-						"2ae38f794emsh66c598ca6e3b64cp1d46ffjsn4d4dc317bdfb",
-					"X-RapidAPI-Host":
-						"ai-weather-by-meteosource.p.rapidapi.com",
-				},
-			}
-		);
-		return data;
-	} catch (error) {
-		console.log(error);
-	}
-};
-
-
-export const getWeatherData = async (lat, lng) => {
-	try {
-		const { data } = await axios.get(
-			`https://ai-weather-by-meteosource.p.rapidapi.com/daily`,
-			{
-				params: {
-					lat: lat,
-					lon: lng,
-					language: "en",
-					units: "metric",
-				},
-				headers: {
 					"X-RapidAPI-Key": import.meta.env.VITE_RAPIDAPI_WEATHER_KEY,
 					"X-RapidAPI-Host":
 						"ai-weather-by-meteosource.p.rapidapi.com",
@@ -73,5 +47,3 @@ export const getWeatherData = async (lat, lng) => {
 		console.log(error);
 	}
 };
-
-
